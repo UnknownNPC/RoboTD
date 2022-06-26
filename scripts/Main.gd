862363609c601e7ea35006ee34ee01d9153913ab
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2022-06-26 20:50:19 +0300

    remove rubbish

--- scripts/Main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-	 	var allSelectable = get_tree().get_nodes_in_group("selectable")
-		for selectable in allSelectable
-			
-			
