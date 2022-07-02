e317c18c1aa6c1b912f70ec30ea9360210be9128
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2022-07-02 15:38:48 +0300

    draft menu

--- scripts/SpawnPoint.gd
+++ scripts/SpawnPoint.gd
@@ -11,9 +11,7 @@ func _ready():
 
 func _on_SelectSprite_input_event(viewport, event, shape_idx):
 	if (event.is_pressed()):
-		
 		$"/root/ScreenUISingleton"._resetUi()
-		
 		print("rendering spawn point")
 		spriteSelect.visible = true
 #		$"/root/Utils".addInfoPanel(self)
