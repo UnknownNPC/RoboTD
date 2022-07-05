edb3b977b5b1a2009b08f49a573ffd4714c38914
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2022-07-05 12:13:16 +0300

    refactor tower code

--- scripts/AssaultTower.gd
+++ scripts/AssaultTower.gd
@@ -1,60 +1,11 @@
 extends "res://scripts/BaseTower.gd"
 
-onready var attackTimer = $AttackTimer
-
-var attackTarget = null
-var canMakeShoot = true
+# enables export var inheritence
+class_name AssaultTower
 
 func _ready():
-	attackTimer.wait_time = attackCooldown
-	currentAnimation.animation = "idle"
 	spriteSelect.scale = Vector2(1.2, 1.2)
 
 	var clickShape = CircleShape2D.new()
 	clickShape.set_radius(16)
 	spriteShape.set_shape(clickShape)
-	
-func _process(delta):
-	if (attackTimer.wait_time != attackCooldown):
-		attackTimer.wait_time = attackCooldown
-	
-	### Attack
-	if (!is_instance_valid(attackTarget)):
-		var allEnemies = get_tree().get_nodes_in_group("enemies")
-		for enemy in allEnemies:
-			var isEnemyInsideRadius = isEnemyInsideAttackRadius(enemy)
-			if  (!enemy.isDead && isEnemyInsideRadius):
-				attackTarget = enemy
-				break
-			else:
-				attackTarget = null
-	else:
-		var isEnemyStilInside = isEnemyInsideAttackRadius(attackTarget)
-		if (!isEnemyStilInside || attackTarget.isDead):
-			attackTarget = null
-			return
-		
-		var isEmemyOnTheLeft = global_position.x - attackTarget.global_position.x > 0
-		if (isEmemyOnTheLeft):
-			currentAnimation.flip_h = true
-		else:
-			currentAnimation.flip_h = false
-
-		# Shoot action
-		if (canMakeShoot):
-			currentAnimation.animation = "fire"
-			currentAnimation.frame = 0
-			
-			attackTarget.add_damage(damageValue)
-			attackTimer.start()
-			canMakeShoot = false
-	
-	if (!is_instance_valid(attackTarget)):
-		currentAnimation.animation = "idle"
-
-func _on_AttackTimer_timeout():
-	canMakeShoot = true
-
-func isEnemyInsideAttackRadius(enemy):
-	return pow((enemy.global_position.x - global_position.x),2) + pow((enemy.global_position.y - global_position.y),2) <= pow(attackRadius, 2)
-
--- scripts/BaseTower.gd
+++ scripts/BaseTower.gd
@@ -27,14 +27,59 @@ onready var animationLv1 = $AnimationLv1
 onready var animationLv2 = $AnimationLv2
 onready var animationLv3 = $AnimationLv3
 
+onready var attackTimer = $AttackTimer
+
 var currentAnimation
 var currentLevel = 1
 var maxLevel = 3
 
+var attackTarget = null
+var canMakeShoot = true
+
 func _ready():
+	attackTimer.wait_time = attackCooldown
+	currentAnimation.animation = "idle"
 	attackRadiusShape.init(attackRadius)
 	initCurrentAnimation()
 	initFullUnitName()
+
+func _process(delta):
+	if (attackTimer.wait_time != attackCooldown):
+		attackTimer.wait_time = attackCooldown
+	
+	### Attack
+	if (!is_instance_valid(attackTarget)):
+		var allEnemies = get_tree().get_nodes_in_group("enemies")
+		for enemy in allEnemies:
+			var isEnemyInsideRadius = isEnemyInsideAttackRadius(enemy)
+			if  (!enemy.isDead && isEnemyInsideRadius):
+				attackTarget = enemy
+				break
+			else:
+				attackTarget = null
+	else:
+		var isEnemyStilInside = isEnemyInsideAttackRadius(attackTarget)
+		if (!isEnemyStilInside || attackTarget.isDead):
+			attackTarget = null
+			return
+		
+		var isEmemyOnTheLeft = global_position.x - attackTarget.global_position.x > 0
+		if (isEmemyOnTheLeft):
+			currentAnimation.flip_h = true
+		else:
+			currentAnimation.flip_h = false
+
+		# Shoot action
+		if (canMakeShoot):
+			currentAnimation.animation = "fire"
+			currentAnimation.frame = 0
+			
+			attackTarget.add_damage(damageValue)
+			attackTimer.start()
+			canMakeShoot = false
+	
+	if (!is_instance_valid(attackTarget)):
+		currentAnimation.animation = "idle"
 
 func _on_SelectSprite_input_event(viewport, event, shape_idx):
 	if (event.is_pressed()):
@@ -92,4 +137,9 @@ func initFullUnitName():
 		fullUnitName = level2NamePrefix + unitName 
 	else:
 		fullUnitName = level3NamePrefix + unitName 
-		
+
+func _on_AttackTimer_timeout():
+	canMakeShoot = true
+
+func isEnemyInsideAttackRadius(enemy):
+	return pow((enemy.global_position.x - global_position.x),2) + pow((enemy.global_position.y - global_position.y),2) <= pow(attackRadius, 2)
