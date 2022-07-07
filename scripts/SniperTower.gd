8bca65f0ae6f53250d8d57cbd219ffe7812435dc
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2022-07-07 21:19:57 +0300

    Add sniper tower

---
+++ scripts/SniperTower.gd
@@ -0,0 +1,12 @@
+extends "res://scripts/BaseTower.gd"
+
+# enables export var inheritence
+class_name SniperTower
+
+func _ready():
+	selectSprite.scale = Vector2(1.2, 1.2)
+
+	var clickShape = CircleShape2D.new()
+	clickShape.set_radius(16)
+	selectShapeCollision.set_shape(clickShape)
+
