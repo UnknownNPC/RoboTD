b0a130b3b2db4747753b56177f742aee7148ffdc
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2022-08-02 09:08:12 +0300

    Add health format

--- scripts/LevelStateUI.gd
+++ scripts/LevelStateUI.gd
@@ -4,13 +4,14 @@ onready var healthValue = $HealthBox/HealthValue
 onready var waveValue = $WaveBox/WaveValue
 onready var energyValue = $EnergyBox/EnergyValue
 
+var healthFormat = "%02d"
 var energyFormat = "%04d"
 var waveFormat = "%02d"
 
 
 func _process(delta):
 	if $"/root/GameProcessState".isReady:
-		healthValue.text = str($"/root/GameProcessState".healthCounter)
+		healthValue.text = str(healthFormat % $"/root/GameProcessState".healthCounter)
 		waveValue.text = (
 			str(waveFormat % $"/root/GameProcessState".currentWaveCounter)
 			+ "/"
